7d96dde5debca891d2c4e9ecf1e7ace031d024e1
olive <utilisateur@pop-os.localdomain>
2022-11-11 00:07:49 +0100

    add resume table and add indexes

--- import_files_to_ES/create_tables.py
+++ import_files_to_ES/create_tables.py
@@ -18,10 +18,10 @@ def create_connection(db_file):
     return conn
 
 
-def create_table(conn, create_table_sql):
-    """ create a table from the create_table_sql statement
+def create_object(conn, create_table_sql):
+    """ create a table or index from the create_table|index_sql statement
     :param conn: Connection object
-    :param create_table_sql: a CREATE TABLE statement
+    :param create_table_sql: a CREATE TABLE|INDEX statement
     :return:
     """
     try:
@@ -34,7 +34,9 @@ def create_table(conn, create_table_sql):
 def main():
     database = r"/home/ubuntu/sqlite/project.db"
 
-    sql_create_review_table = """ CREATE TABLE review (
+    ##############################################
+    # permanent tables
+    sql_create_review_table = """ CREATE TABLE IF NOT EXISTS review (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     lastupdated_date TIMESTAMP default current_timestamp,
     company_id INTEGER,
@@ -51,12 +53,22 @@ def main():
     user_country VARCHAR,
     response_company VARCHAR,
     response_company_date DATE,
-    UNIQUE(reviewUnitId),
-    UNIQUE(company_id, user_id),
     FOREIGN KEY (company_id) REFERENCES company (id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
     ); """
 
-    sql_create_company_table = """CREATE TABLE company (
+    sql_create_review_index1 = """
+    CREATE UNIQUE INDEX rev_index1
+    ON review(company_id, user_id);
+    """
+
+    sql_create_review_index2 = """
+    CREATE UNIQUE INDEX rev_index2
+    ON review(reviewUnitId);
+    """
+
+    sql_create_company_table = """CREATE TABLE IF NOT EXISTS company (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     lastupdated_date TIMESTAMP default current_timestamp,
     subSectorLevel1_id INTEGER,
@@ -72,32 +84,125 @@ def main():
     contact_website VARCHAR,
     contact_email VARCHAR,
     contact_phone VARCHAR,
-    reviews_resume VARCHAR,
-    reviews_keywords VARCHAR,
-    UNIQUE(businessUnitId),
     UNIQUE(website),
-    UNIQUE(display_name),
     FOREIGN KEY (subSectorLevel1_id) REFERENCES subSectorLevel1 (id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
     );"""
 
-    sql_create_subSector_table = """CREATE TABLE subSectorLevel1 (
+    sql_create_company_index1 = """
+    CREATE UNIQUE INDEX comp_index1
+    ON company(businessUnitId);
+    """
+
+    sql_create_company_index2 = """
+    CREATE UNIQUE INDEX comp_index2
+    ON company(display_name);
+    """
+
+    sql_create_subSector_table = """CREATE TABLE IF NOT EXISTS subSectorLevel1 (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     lastupdated_date TIMESTAMP default current_timestamp,
     sector_id INTEGER,
     label_name VARCHAR,
     display_name VARCHAR,
-    UNIQUE(label_name),
-    UNIQUE(display_name),
     FOREIGN KEY (sector_id) REFERENCES sector (id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
     );"""
 
-    sql_create_sector_table = """CREATE TABLE sector (
+    sql_create_subSector_index1 = """
+    CREATE UNIQUE INDEX subS_index1
+    ON subSectorLevel1(label_name);
+    """
+
+    sql_create_subSector_index2 = """
+    CREATE UNIQUE INDEX subS_index2
+    ON subSectorLevel1(display_name);
+    """
+
+    sql_create_sector_table = """CREATE TABLE IF NOT EXISTS sector (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     lastupdated_date TIMESTAMP default current_timestamp,
     label_name VARCHAR,
+    display_name VARCHAR
+    );"""
+
+    sql_create_sector_index1 = """
+    CREATE UNIQUE INDEX sector_index1
+    ON sector(label_name);
+    """
+
+    sql_create_sector_index2 = """
+    CREATE UNIQUE INDEX sector_index2
+    ON sector(display_name);
+    """
+
+    sql_create_resume_table = """CREATE TABLE IF NOT EXISTS review_resume (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    lastupdated_date TIMESTAMP default current_timestamp,
+    company_id INTEGER,
+    resume_type VARCHAR,
+    resume_text VARCHAR,
+    resume_keywords VARCHAR,
+    FOREIGN KEY (company_id) REFERENCES company (id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+    );"""
+
+    sql_create_resume_index1 = """
+    CREATE UNIQUE INDEX resume_index1
+    ON review_resume(company_id, resume_type);
+    """
+
+    ############################################################
+    # temporary tables
+    sql_temp_create_review_table = """ CREATE TABLE IF NOT EXISTS ztemp_review (
+    lastupdated_date TIMESTAMP default current_timestamp,
+    businessUnitId VARCHAR,
+    reviewUnitId VARCHAR,
+    review_title VARCHAR,
+    review_text VARCHAR,
+    experience_date DATE,
+    publish_date DATE,
+    rating INTEGER,
+    isVerified VARCHAR,
+    user_id VARCHAR,
+    user_name VARCHAR,
+    user_nbOfReviews INTEGER,
+    user_country VARCHAR,
+    response_company VARCHAR,
+    response_company_date DATE
+    ); """
+
+    sql_temp_create_company_table = """CREATE TABLE IF NOT EXISTS ztemp_company (
+    lastupdated_date TIMESTAMP default current_timestamp,
+    subSector1_label_name VARCHAR,
+    businessUnitId VARCHAR,
+    website VARCHAR,
     display_name VARCHAR,
-    UNIQUE(label_name),
-    UNIQUE(display_name)
+    score INTEGER,
+    logoUrl VARCHAR,
+    postal_address VARCHAR,
+    postal_city VARCHAR,
+    postal_zipCode VARCHAR,
+    postal_country VARCHAR,
+    contact_website VARCHAR,
+    contact_email VARCHAR,
+    contact_phone VARCHAR
+    );"""
+
+    sql_temp_create_subSector_table = """CREATE TABLE IF NOT EXISTS ztemp_subSectorLevel1 (
+    lastupdated_date TIMESTAMP default current_timestamp,
+    sector_label_name VARCHAR,
+    label_name VARCHAR,
+    display_name VARCHAR
+    );"""
+
+    sql_temp_create_sector_table = """CREATE TABLE IF NOT EXISTS ztemp_sector (
+    lastupdated_date TIMESTAMP default current_timestamp,
+    label_name VARCHAR,
+    display_name VARCHAR
     );"""
 
 
@@ -106,17 +211,51 @@ def main():
 
     # create tables
     if conn is not None:
+        ############################
         # create review table
-        create_table(conn, sql_create_review_table)
+        create_object(conn, sql_create_review_table)
 
         # create company table
-        create_table(conn, sql_create_company_table)
+        create_object(conn, sql_create_company_table)
 
         # create subSector table
-        create_table(conn, sql_create_subSector_table)
+        create_object(conn, sql_create_subSector_table)
 
         # create sector table
-        create_table(conn, sql_create_sector_table)
+        create_object(conn, sql_create_sector_table)
+
+        # create resume table
+        create_object(conn, sql_create_resume_table)
+
+        ###############################
+
+        # create index
+        create_object(conn, sql_create_review_index1)
+        create_object(conn, sql_create_review_index2)
+
+        create_object(conn, sql_create_company_index1)
+        create_object(conn, sql_create_company_index2)
+
+        create_object(conn, sql_create_subSector_index1)
+        create_object(conn, sql_create_subSector_index2)
+
+        create_object(conn, sql_create_sector_index1)
+        create_object(conn, sql_create_sector_index2)
+
+        create_object(conn, sql_create_resume_index1)
+
+        ###############################
+        # create temp review table
+        create_object(conn, sql_temp_create_review_table)
+
+        # create temp company table
+        create_object(conn, sql_temp_create_company_table)
+
+        # create temp subSector table
+        create_object(conn, sql_temp_create_subSector_table)
+
+        # create temp sector table
+        create_object(conn, sql_temp_create_sector_table)
     else:
         print("Error! cannot create the database connection.")
 
--- import_files_to_ES/insert_data.py
+++ import_files_to_ES/insert_data.py
@@ -4,17 +4,38 @@ from elasticsearch import Elasticsearch
 from elasticsearch_dsl import Search
 
 client = Elasticsearch(hosts = "http://@localhost:9200")
-s = Search(using=client, index="cat_1")
+sCat = Search(using=client, index="cat_1")
+sComp = Search(using=client, index="comp_1")
+sRev = Search(using=client, index="rev_1")
 
-scanListCat = [hit.to_dict() for hit in s.scan()][0]["topCategories"]
-listCat = str([(x["categoryId"], x["displayName"]) for x in scanListCat]).strip("[]")
 
-scanDictSubCat = [hit.to_dict() for hit in s.scan()][0]["SubCategories"]
+scanListCat = [hit.to_dict() for hit in sCat.scan()][0]["topCategories"]
+listCat = [(x["categoryId"], x["displayName"]) for x in scanListCat]
+
+scanDictSubCat = [hit.to_dict() for hit in sCat.scan()][0]["subCategories"]
 listSubCat = []
 for key, value in scanDictSubCat.items():
     for d in value:
         listSubCat.append((d["parentId"], d["categoryId"], d["displayName"]))
-strSubCat = str(listSubCat).strip("[]")
+
+scanListComp = [hit.to_dict() for hit in sComp.scan()][0]["businesses"]
+listComp = [(
+    x["businessUnitId"], x["identifyingName"], x["displayName"],
+    x["trustScore"], x["logoUrl"], x["location"]["address"],
+    x["location"]["city"], x["location"]["zipCode"], x["location"]["country"],
+    x["contact"]["website"], x["contact"]["email"], x["contact"]["phone"]
+    ) for x in scanListComp]
+
+scanListRev = [hit.to_dict() for hit in sRev.scan()][0]
+listRev = [(
+    x["businessUnit"]["id"], x["reviews"]["id"], x["reviews"]["title"], x["reviews"]["text"],
+    x["reviews"]["dates"]["experiencedDate"], x["reviews"]["dates"]["publishedDate"], x["reviews"]["text"],
+    x["reviews"]["rating"], x["reviews"]["verification"]["isVerified"], x["reviews"]["consumer"]["id"],
+    x["reviews"]["consumer"]["displayName"], x["reviews"]["consumer"]["numberOfReviews"],
+    x["reviews"]["consumer"]["countryCode"], x["reviews"]["reply"]["message"], x["reviews"]["reply"]["publishedDate"]
+    ) for x in scanListRev]
+
+
 
 
 
@@ -34,15 +55,27 @@ def create_connection(db_file):
     return conn
 
 
-def insert_data(conn, insert_sql):
-    """ insert date from the insert_sql statement
+def insert_data_temp(conn, insert_sql, list_to_insert):
+    """ insert data from the insert_sql statement
     :param conn: Connection object
     :param insert_sql: a INSERT INTO statement
     :return:
     """
     try:
         c = conn.cursor()
-        c.execute(insert_sql)
+        c.executemany(insert_sql, list_to_insert)
+    except Error as e:
+        print(e)
+
+def insert_data_perm(conn, insert_sql):
+    """ insert data from the insert_sql statement
+    :param conn: Connection object
+    :param insert_sql: a INSERT INTO statement
+    :return:
+    """
+    try:
+        d = conn.cursor()
+        d.execute(insert_sql)
     except Error as e:
         print(e)
 
@@ -50,26 +83,88 @@ def insert_data(conn, insert_sql):
 def main():
     database = r"/home/ubuntu/sqlite/project.db"
 
+    sql_insert_temp_cat_table = """
+    INSERT INTO ztemp_sector (label_name, display_name)
+    VALUES (?, ?)
+    ;"""
+
     sql_insert_cat_table = """
     INSERT INTO sector (label_name, display_name)
-    SELECT * FROM
-    (
-        VALUES {}
-    ) t1
-    LEFT JOIN sector t2 ON t1.{} = t2.label_name
-    WHERE t2.label_name is null
-    ;""".format(listCat, scanListCat[0]["categoryId"])
+    SELECT label_name, display_name from ztemp_sector
+    WHERE label_name not in (select label_name from sector)
+    ;"""
+
+    sql_insert_temp_subCat_table = """
+    INSERT INTO ztemp_subSectorLevel1 (sector_label_name, label_name, display_name)
+    VALUES (?, ?, ?)
+    ;"""
 
     sql_insert_subCat_table = """
     INSERT INTO subSectorLevel1 (sector_id, label_name, display_name)
-    SELECT t3.id, {}, {} FROM
+
+    SELECT t2.sector_id, t1.label_name, t1.display_name 
+    FROM ztemp_subSectorLevel1 t1
+    LEFT JOIN sector t2 ON t2.label_name = t1.sector_label_name
+    WHERE t1.label_name NOT IN
     (
-        VALUES {}
-    ) t1
-    LEFT JOIN subSectorLevel1 t2 on t2.label_name = t1.{}
-    LEFT JOIN sector t3 ON t3.label_name = t1.{}
-    WHERE t2.label_name is null
-    ;""".format(listSubCat[0][1], listSubCat[0][2], strSubCat, listSubCat[0][0], listSubCat[0][0])
+        select label_name from subSectorLevel1
+    )
+    ;"""
+
+    sql_insert_temp_company_table = """
+    INSERT INTO ztemp_company (
+    subSector1_label_name, businessUnitId, website, display_name,
+    score, logoUrl, postal_address, postal_city, postal_zipCode, 
+    postal_country, contact_website, contact_email, contact_phone
+    )
+    VALUES (?, ?, ?)
+    ;"""
+
+    sql_insert_company_table = """
+    INSERT INTO company (
+    subSectorLevel1_id, businessUnitId, website, display_name,
+    score, logoUrl, postal_address, postal_city, postal_zipCode, 
+    postal_country, contact_website, contact_email, contact_phone
+    )
+    SELECT
+    t2.id, businessUnitId, website, display_name,
+    score, logoUrl, postal_address, postal_city, postal_zipCode, 
+    postal_country, contact_website, contact_email, contact_phone
+
+    FROM ztemp_company t1
+    LEFT JOIN subSectorLevel1 t2 ON t2.label_name = t1.subSector1_label_name
+
+    WHERE t1.businessUnitId NOT IN (select businessUnitId from company)
+    ;"""
+
+    sql_insert_temp_review_table = """
+    INSERT INTO ztemp_review (
+    businessUnitId, reviewUnitId, review_title, review_text,
+    experience_date, publish_date, rating, isVerified, user_id, 
+    user_name, user_nbOfReviews, user_country, response_company,
+    response_company_date
+    )
+    VALUES (?, ?, ?)
+    ;"""
+    
+    sql_insert_review_table = """
+    INSERT INTO review (
+    company_id, reviewUnitId, review_title, review_text,
+    experience_date, publish_date, rating, isVerified, user_id, 
+    user_name, user_nbOfReviews, user_country, response_company,
+    response_company_date
+    )
+    SELECT
+    t2.id, reviewUnitId, review_title, review_text,
+    experience_date, publish_date, rating, isVerified, user_id, 
+    user_name, user_nbOfReviews, user_country, response_company,
+    response_company_date
+
+    FROM ztemp_review t1
+    LEFT JOIN company t2 ON t2.label_name = t1.company_label_name
+
+    WHERE t1.reviewUnitId NOT IN (select reviewUnitId from review)
+    ;"""
 
 
     # create a database connection
@@ -77,11 +172,30 @@ def main():
 
     # create tables
     if conn is not None:
+
+        # insert into temp cat table
+        insert_data_temp(conn, sql_insert_temp_cat_table, listCat)
+
         # insert into cat table
-        insert_data(conn, sql_insert_cat_table)
+        insert_data_perm(conn, sql_insert_cat_table)
+
+        # insert into temp subCat table
+        insert_data_temp(conn, sql_insert_temp_subCat_table, listSubCat)
 
         # insert into subCat table
-        insert_data(conn, sql_insert_subCat_table)
+        insert_data_perm(conn, sql_insert_subCat_table)
+
+        # insert into temp company table
+        insert_data_temp(sql_insert_temp_company_table, listComp)
+
+        # insert into company table
+        insert_data_perm(sql_insert_company_table)
+
+        # insert into temp review table
+        insert_data_temp(sql_insert_temp_review_table, listRev)
+
+        # insert into review table
+        insert_data_perm(sql_insert_review_table)
 
     else:
         print("Error! cannot create the database connection.")
