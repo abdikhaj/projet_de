7145d95b435a971ab4f8e0c1d7746450ed0b4475
oliviercodeca <oliviercodeca31@gmail.com>
2022-11-24 10:37:43 +0100

    delete lines

--- html_to_es/scraping_trustpilot.py
+++ html_to_es/scraping_trustpilot.py
@@ -1,54 +1,57 @@
-import requests
-import re
+import requests, re, os, json
 from custom_vars import vars
-import os
 from tqdm import tqdm
 
 # steps:
-# 1- create all categories files
-# 2- create companies files from the 17 default categories
-# 3- create reviews files from the 17 default categories
-# 4- third create a script to update a company if the company is not in the default companies
+# 1- create all categories file
+# 2- create  all companies file
 
 
 
-# create all categories 
+# create the file categories.json in bulk Elasticsearch format
 urlCat = 'https://fr.trustpilot.com/categories'
 html_respCat = requests.get(url=urlCat)
 html_substr_cat = re.findall('"subCategories"\:\{.+\]\}\]\}\,"languages"', html_respCat.text)
-html_cat = '{"create":{"_index":"cat_1","_id":"1"}}\n' +'{' + html_substr_cat[0][:-12] + '}\n'
+html_cat_data = '{' + html_substr_cat[0][:-12] + '}'
+html_cat = '{"create":{"_index":"cat_1","_id":"1"}}\n' + str(html_cat_data) + '\n'
 with open("{file_path}/categories.json".format(file_path=vars("file_path_push_to_es")), "w", encoding='utf-8') as file:
     file.write(html_cat)
 
-# create companies files from the 17 default categories
-json_name = "companies_default"
+# create a list with all the categories
+list_cat = []
+dict_cat = json.loads(html_cat_data)
+for k_cat in dict_cat["subCategories"]:
+    for l in dict_cat["subCategories"][k_cat]:
+        list_cat.append(l["categoryId"])
 
-# delete the comapny file if exsts
+
+# delete the company file if exsts
+json_name = "companies_default"
 if os.path.exists("{file_path}/{json_name}.json".format(json_name=json_name, file_path=vars("file_path_push_to_es"))):
   os.remove("{file_path}/{json_name}.json".format(json_name=json_name, file_path=vars("file_path_push_to_es")))
-# append the company file
 
-for cat_name in tqdm(vars("default_category_name")):
-    for num_page in tqdm(range(1,50), leave=False):
+# create companies file
+html_all_comp = ""
+for cat_name in tqdm(list_cat):
+    
+    # get the last num html page to loop on page
+    urlCompLast = 'https://fr.trustpilot.com/categories/{cat_name}'.format(cat_name=cat_name)
+    html_respCompLast = requests.get(url=urlCompLast)
+    m = re.search('"totalPages"\:(\d+)\,"perPage"', html_respCompLast.text)
+    if m:
+        html_last_num_page = int(m.group(1))
+
+    # loop on page
+    for num_page in range(1, html_last_num_page+1):
         doc_name = cat_name + "_" + str(num_page)
         urlComp = 'https://fr.trustpilot.com/categories/{cat_name}?page={num_page}'.format(cat_name=cat_name, num_page=num_page)
         html_respComp = requests.get(url=urlComp)
         if html_respComp.status_code == 200:
             html_substr_comp = re.findall('"businesses"\:\[\{"businessUnitId".+\}\]\}\]\,"totalPages"', html_respComp.text)
             html_comp = r'{"create":{"_index":"comp_1","_id":"' + '{doc_name}"}}\n'.format(doc_name=doc_name) + '{' + html_substr_comp[0][:-13] + '}\n'
-            with open("{file_path}/{json_name}.json".format(json_name=json_name, file_path=vars("file_path_push_to_es")), "a", encoding='utf-8') as file:
-                file.write(html_comp)
+            html_all_comp += html_comp
         else:
             break
 
-
-"""
-urlRev = 'https://fr.trustpilot.com/review/grandluxuryhotels.com'
-html_respRev = requests.get(url=urlRev)
-html_substr_rev1 = re.findall('"reviews"\:.+\}\]\,"products"', html_respRev.text)[0]
-html_substr_rev = re.findall('"businessUnit"\:\{"id"\:"\w+"', html_respRev.text)[0] + '},' + html_substr_rev1
-html_rev = '{"create":{"_index":"rev_1","_id":"1"}}\n' +'{' + html_substr_rev[:-11] + '}\n'
-
-with open("{file_path}/reviews_1.json".format(file_path=vars("file_path_push_to_es")), "w", encoding='utf-8') as file:
-    file.write(html_rev)
-"""
+with open("{file_path}/{json_name}.json".format(json_name=json_name, file_path=vars("file_path_push_to_es")), "a", encoding='utf-8') as file:
+    file.write(html_all_comp)
